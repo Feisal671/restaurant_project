6674efb5313b254f99d2903abf8cf2e7ea74c95b
unknown <salmanbwoy61@gmail.com>
2026-04-08 08:08:54 +0300

    Add menu list view, url and templates

--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import MenuItem
+
+def menu_list(request):
+    items = MenuItem.objects.all()
+    return render(request, 'menu/menu_list.html',{'items': items})
